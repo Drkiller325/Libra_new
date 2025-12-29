3cd53f33d7cd46b552622092f44faa65dc11531e
Ahmed <ahmed-2008-04@hotmail.com>
2025-12-29 18:13:30 +0200

    Configure dependancy injection

---
+++ Application/Extentions/ApplicationDependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using Autofac.Integration.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+
+namespace Application.Extentions
+{
+    public class ApplicationDependencyInjection
+    {
+
+        public static void RegisterApplication(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(typeof(ApplicationDependencyInjection).Assembly)
+                .Where(t => t.GetInterfaces().Any(i => i.IsClosedTypeOf(typeof(IValidator<>))))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterHubs(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ Web/App_Start/DependencyConfig.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MediatR;
+using Autofac.Integration.Mvc;
+using System.Reflection;
+using Application.Extentions;
+using System.Web.UI;
+using FluentValidation;
+
+namespace Web.App_Start
+{
+    public class DependencyConfig
+    {
+        public static void RegisterDependencies()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<Mediator>().As<IMediator>().InstancePerLifetimeScope();
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterAssemblyTypes(typeof(ApplicationDependencyInjection).Assembly)
+                .Where(t => t.GetInterfaces().Any(i => i.IsClosedTypeOf(typeof(IValidator<>))))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            
+        }
+    }
+}
